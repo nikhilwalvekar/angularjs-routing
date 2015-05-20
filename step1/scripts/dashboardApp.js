1b3c2f2bc228eed5fe98788b9a387cdb87aad4d4
Nikhil Walvekar <nikhil.walvekar@synerzip.com>
2015-05-19 23:48:47 -0700

    Added basic step1. all contents are present in separate html files and shown using ng-include.

---
+++ step1/scripts/dashboardApp.js
@@ -0,0 +1,12 @@
+var app = angular.module("dashboardApp", ['ngRoute']);
+
+app.controller('appController', appController)
+	.config(appConfig);
+
+var appConfig = function() {
+
+}
+
+var appController = function($scope) {
+
+}
