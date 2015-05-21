25adea6c8c0f495af77e4c8e0831c4018ec116f3
Nikhil Walvekar <nikhil.walvekar@synerzip.com>
2015-05-20 22:57:30 -0700

    Added basic HTML SPA to step1
    Layout is built using ng-include

---
+++ README.md
@@ -0,0 +1,12 @@
+# Angular routing
+
+1. Basic SPA
+Everything is separate page and 
+
+2. 
+
+3. Using urlRouter
+
+4. Using stateProvider
+4.1 Using named views
+4.2 Using nested views
--- step1/scripts/dashboardApp.js
+++ step1/scripts/dashboardApp.js
@@ -1,12 +1,10 @@
 var app = angular.module("dashboardApp", ['ngRoute']);
 
-app.controller('appController', appController)
-	.config(appConfig);
-
 var appConfig = function() {
 
 }
-
 var appController = function($scope) {
-
 }
+
+app.controller('appController', appController)
+	.config(appConfig);
